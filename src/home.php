cfb877a0c13a37f8185f2644870ac355946017ab
Alan Tavares <alan_tj@hotmail.com.br>
2021-10-04 17:01:11 -0300

    Template

---
+++ src/home.php
@@ -0,0 +1,20 @@
+<?php
+    require_once '../generics/page.php';
+    $page = new page();
+    $page->head();
+ ?>
+ <div class="container-fluid">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
+                                </div>
+                                <div class="card-body">
+                                    <p>Bem vindo!</p>
+                                  
+                                </div>
+                            </div>
+
+</div>
+<?php 
+    $page->foot();
+?>
